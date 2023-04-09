208036dee341282144ff1ff596eb4a2042f68f77
landot <timowland@gmail.com>
2023-04-08 18:02:45 -0700

    use hashrouter to get project working in gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import TodoList from './components/todoList';
@@ -36,13 +36,13 @@ function App() {
       <nav>
         <ul style={{listStyle: "none"}}>
           <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/todolist">Todo List App</a></li>
-          <li><a href="/pomodoro">Pomodoro</a></li>
-          <li><a href="/counters">Counters</a></li>
+          <li><a href="/#/about">About</a></li>
+          <li><a href="/#/todolist">Todo List App</a></li>
+          <li><a href="/#/pomodoro">Pomodoro</a></li>
+          <li><a href="/#/counters">Counters</a></li>
         </ul>
       </nav>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/"/>
           <Route path="/about" element={<About/>}/>
@@ -50,7 +50,7 @@ function App() {
           <Route path="/pomodoro" element={<Pomodoro/>}/>
           <Route path="/counters" element={<Counters/>}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
