94b5d458ddec4523ebcf27c1804a18c7a3c4f72e
landot <timowland@gmail.com>
2023-04-08 18:22:12 -0700

    install mui

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,14 +1,15 @@
-import React, { ButtonHTMLAttributes } from "react"
+import React, { ButtonHTMLAttributes } from "react";
+import { Button } from "@mui/material";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     count: number;
     dataTestId: string;
 }
 
-export default function Button({count, onClick, dataTestId}: ButtonProps) {
+export default function CounterButton({count, onClick, dataTestId}: ButtonProps) {
     return (
-        <button onClick={onClick} data-testid={dataTestId}>
-            These button has been clicked {count} times
-        </button>
+        <Button style={{display: "block", margin: "auto"}}variant="contained" onClick={onClick} data-testid={dataTestId}>
+            Button clicks: {count} times
+        </Button>
     )
 }
--- src/components/counters.tsx
+++ src/components/counters.tsx
@@ -5,8 +5,10 @@ import CombinedClickCounter from './combinedClickCounter';
 export default function Counters() {
     return (
         <div>
+            <p>buttons with individual click count:</p>
             <ClickCounter name='counter-1'/>
             <ClickCounter name='counter-2'/>
+            <p>buttons with shared click count:</p>
             <CombinedClickCounter data-testid='combined-counter'/>
         </div>
     )
